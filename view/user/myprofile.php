33818b976ca55ada8ec14437d762e42a9bc37f6b
triguz <trigun271289@hotmail.it>
2016-01-21 19:22:50 +0100

    Versione 1.0

--- view/user/myprofile.php
+++ view/user/myprofile.php
@@ -8,22 +8,7 @@ include_once './model/database.php';
 			<div class="clear"></div>
                         <h2>Il tuo profilo</h2> 
 <?php 
-$username= $_SESSION['username'];
-$db= database::getInstance();
-$query= "SELECT email, name, surname, street, number, city, state FROM user WHERE username= '$username'";
-$stmt->prepare($query);
-        
-        if (!$stmt){ 
-            return false;
-        }
-        
-        if(!$stmt->execute())
-        {
-            $stmt->close();
-            $mysqli->close();
-            return false;
-        }
-else{
+userDB::instance()->existUser($_POST['username'], $_POST['password']);
 while($row= $result->fetch_row())
 {
 $email=$row[0];
@@ -33,8 +18,7 @@ $street=$row[3];
 $number=$row[4];
 $city=$row[5];
 $state=$row[6];
-database::chiudiDB();
-}}
+}
 ?>
 <p>Nome: <?php echo $name." ".$surname ?> </p>
 <p>Email: <?php echo $email; ?></p>
