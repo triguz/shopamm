c339a0efa28c0d86a19e21b1576f8b5ee543f6c4
triguz <trigun271289@hotmail.it>
2016-01-21 18:14:33 +0100

    Versione 1.0

--- view/user/myprofile.php
+++ view/user/myprofile.php
@@ -9,28 +9,11 @@ include_once './model/database.php';
             <div id="pageContentCenter">
 			<div class="clear"></div>
                         <h2>Il tuo profilo</h2> 
-                            <?php $username = $_SESSION['username'];
-           $db = shopamm::instance();
-$query = "SELECT email, name, surname, street, number, city, state FROM user WHERE username= '$username'";
-$result = $db->query($query);
-    if ($db->errno > 0) {
-        echo "Errore nell'esecuzione della query $db->errno : $db->error", 0;
-} else {
-    while ($row = $result->fetch_row()) {
-        $email = $row[0];
-        $name = $row[1];
-        $surname = $row[2];
-        $street = $row[3];
-        $number = $row[4];
-        $city = $row[5];
-        $state = $row[6];
-        shopamm::chiudiDB();
-    }
-} ?> 
+                        
+                            
 <p>Nome:  <?php $user->getName() ?>
-<p>Email: <?php echo $email; ?></p> 
-<p>Indirizzo: <?php echo $street . " " . $number . ", " . $city . ", " . $state; ?></p>
-                </form>         
+
+                    
             </div>
         </ul>
   </div>
