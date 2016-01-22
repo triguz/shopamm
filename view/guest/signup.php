4157693756cd188b53462c4f7993f0cf39ba8890
triguz <trigun271289@hotmail.it>
2016-01-22 19:06:24 +0100

    Versione 1.0

--- view/guest/signup.php
+++ view/guest/signup.php
@@ -4,41 +4,43 @@
             <div id="pageContentCenter">
                 <form action="index.php?action=signup" method="post">
                     <div id="add_err_registration"></div>
-                    
-                    <label>Email:</label>
-                    <input type="text" id="email" name="email" value="" />
                     <div class="clear"></div>
-                    <label>Username:</label>
-                    <input type="text" id="r_username" name="username" value="" />
+                    <ul>
+                    <li><label>Email:</label></li> 
+                    <li><input type="text" id="email" name="email" value="" /></li>
                     <div class="clear"></div>
-                    <label>Password:</label>
-                    <input type="password" id="r_password" name="password" value="" />
+                    <li><label>Username:</label></li>
+                    <li><input type="text" id="r_username" name="username" value="" /></li>
                     <div class="clear"></div>
-                    <label>Confirm the password:</label>
-                    <input type="password" id="confirm" name="confirm" value="" />
+                    <li><label>Password:</label></li>
+                    <li><input type="password" id="r_password" name="password" value="" /></li>
                     <div class="clear"></div>
-                    <label>Name:</label>
-                    <input type="text" id="name" name="name" value="" />
+                    <li><li><label>Confirm the password:</label></li>
+                    <li><input type="password" id="confirm" name="confirm" value="" /></li>
                     <div class="clear"></div>
-                    <label>Surname:</label>
-                    <input type="text" id="surname" name="surname" value="" />
+                    <li><label>Name:</label></li>
+                    <li><input type="text" id="name" name="name" value="" /></li>
                     <div class="clear"></div>
-                    <label>Street:</label>
-                    <input type="text" id="street" name="street" value="" />
+                    <li><label>Surname:</label></li>
+                    <li><input type="text" id="surname" name="surname" value="" /></li>
                     <div class="clear"></div>
-                    <label>Number:</label>
-                    <input type="text" id="number" name="number" value="" />
+                    <li><label>Street:</label></li>
+                    <li><input type="text" id="street" name="street" value="" /></li>
                     <div class="clear"></div>
-                    <label>City:</label>
-                    <input type="text" id="city" name="city" value="" />
+                    <li><label>Number:</label></li>
+                    <li><input type="text" id="number" name="number" value="" /></li>
                     <div class="clear"></div>
-                    <label>Postal Code:</label>
-                    <input type="text" id="postalCode" name="postalCode" value="" />
+                    <li><label>City:</label></li>
+                    <li><input type="text" id="city" name="city" value="" /></li>
                     <div class="clear"></div>
-                    <label>State:</label>
-                    <input type="text" id="state" name="state" value="" />
+                    <li><label>Postal Code:</label></li>
+                    <li><input type="text" id="postalCode" name="postalCode" value="" /></li>
                     <div class="clear"></div>
-                    <input type="submit" id="submit_registration" name="submit_registration" value="Sign Up" />
+                    <li><label>State:</label></li>
+                    <li><input type="text" id="state" name="state" value="" /></li>
+                    <div class="clear"></div>
+                    <li><input type="submit" id="submit_registration" name="submit_registration" value="Sign Up" /></li>
+                    </ul>
                 </form>         
             </div>
         </ul>
