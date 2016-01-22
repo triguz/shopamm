14655dc87aee84ed13fe52233c831bacaa18b751
triguz <trigun271289@hotmail.it>
2016-01-22 17:04:04 +0100

    Versione 1.0

--- view/user/myprofile.php
+++ view/user/myprofile.php
@@ -1,12 +1,7 @@
 <?php
 include_once './model/database.php'
 ?>
-<div class="main">
-  <div id="featured_slide_">
-        <ul id="content">
-            <div id="pageContentCenter">
-			<div class="clear"></div>
-                        <h2>Il tuo profilo</h2> 
+
 <?php 
 $mysqli = database::getInstance()->databaseConnection();
 if (!isset($mysqli)) 
@@ -25,15 +20,28 @@ $res = $mysqli->query($sql) or trigger_error($mysqli->error."[$sql]");
 while($row = $res->fetch_assoc()) {
     $arrChartData[] = $row;
 }
-$name=$arrChartData[1];
+$name=$arrChartData[4];
+$surname=$arrChartData[5];
+$email=$arrChartData[1];
+$street=$arrChartData[6];
+$number=$arrChartData[7];
+$city=$arrChartData[8];
+$state=$arrChartData[10];
         $mysqli->close();
         
         return true;
 ?>
+<div class="main">
+  <div id="featured_slide_">
+        <ul id="content">
+            <div id="pageContentCenter">
+			<div class="clear"></div>
+                        <h2>Il tuo profilo</h2> 
+<ul id="featured_slide_Content">
 <p>Nome: <?php echo $name." ".$surname ?> </p>
 <p>Email: <?php echo $email; ?></p>
 <p>Indirizzo: <?php echo $street." ".$number.", ".$city.", ".$state; ?></p> 
-                            
+</ul>                           
 
 
                     
