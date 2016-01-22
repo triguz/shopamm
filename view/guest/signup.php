719730e8174afb6464c7723b66fd249dabcd3914
triguz <trigun271289@hotmail.it>
2016-01-22 19:09:57 +0100

    Versione 1.0

--- view/guest/signup.php
+++ view/guest/signup.php
@@ -8,14 +8,14 @@
                     <ul>
                     <li><label>Email:</label></li> 
                     <li><input type="text" id="email" name="email" value="" /></li>
-                    
+                    <div class="cl">&nbsp;</div>
                     <li><label>Username:</label></li>
                     <li><input type="text" id="r_username" name="username" value="" /></li>
-                    
+                    <div class="cl">&nbsp;</div>
                     <li><label>Password:</label></li>
                     <li><input type="password" id="r_password" name="password" value="" /></li>
-                    
-                    <li><li><label>Confirm the password:</label></li>
+                    <div class="clear"></div>
+                    <li><label>Confirm the password:</label></li>
                     <li><input type="password" id="confirm" name="confirm" value="" /></li>
                     <div class="clear"></div>
                     <li><label>Name:</label></li>
