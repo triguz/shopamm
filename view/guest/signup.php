b8116be52ad0950f1a2b868cba5e804ed0cf8f3d
triguz <trigun271289@hotmail.it>
2016-01-22 19:07:57 +0100

    Versione 1.0

--- view/guest/signup.php
+++ view/guest/signup.php
@@ -8,13 +8,13 @@
                     <ul>
                     <li><label>Email:</label></li> 
                     <li><input type="text" id="email" name="email" value="" /></li>
-                    <div class="clear"></div>
+                    
                     <li><label>Username:</label></li>
                     <li><input type="text" id="r_username" name="username" value="" /></li>
-                    <div class="clear"></div>
+                    
                     <li><label>Password:</label></li>
                     <li><input type="password" id="r_password" name="password" value="" /></li>
-                    <div class="clear"></div>
+                    
                     <li><li><label>Confirm the password:</label></li>
                     <li><input type="password" id="confirm" name="confirm" value="" /></li>
                     <div class="clear"></div>
